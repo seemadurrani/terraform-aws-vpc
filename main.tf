d8de8eb44037750cad5390b03286da34f84d53a9
Seema Durrani <seemadurrani2799@gmail.com>
2025-08-27 08:27:47 -0400

    provision vpc, subnets and the internet gateway

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source              = "./modules/vpc"
+  vpc_cidr_block      = var.vpc_cidr_block
+  public_subnet_cidrs = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  azs                 = var.azs
+}
+
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr_block" {
+}
+
+variable "public_subnet_cidrs" { 
+  type = list(string) 
+}
+
+variable "private_subnet_cidrs" { 
+  type = list(string) 
+}
+
+variable "azs" { 
+  type = list(string) 
+}
+
