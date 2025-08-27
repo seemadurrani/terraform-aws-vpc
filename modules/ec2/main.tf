68e4a28817636437163ab50e14fc5ae7c831a0db
Seema Durrani <seemadurrani2799@gmail.com>
2025-08-27 09:00:53 -0400

    Implement EC2 instance with security group allowing HTTP

---
+++ modules/ec2/main.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+
+  tags = {
+    Name = "nginx-server"
+  }
+}
+
