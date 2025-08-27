48bd666c97319df1fb1bb8c6c78517902edd7a70
Seema Durrani <seemadurrani2799@gmail.com>
2025-08-27 09:33:28 -0400

    Wire EC2 module in root configuration and expose public IP

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "ec2" {
   source         = "./modules/ec2"
   vpc_id         = module.vpc.vpc_id
   subnet_id      = module.vpc.public_subnet_ids[0]
-  ami_id         = var.ami_id
+  ami_id = data.aws_ami.amazon_linux.id
   instance_type  = var.instance_type
 }
 
